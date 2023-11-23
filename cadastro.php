60dbe5c30c5aab98299763e54d074ccbe840a526
Ana Karolina <anak54497@gmail.com>
2023-11-23 16:31:11 -0300

    Atualização

--- cadastro.php
+++ cadastro.php
@@ -47,8 +47,9 @@
                 <button class="btn btn-outline-success b l" type="submit">Logout</button>
               </div>
             </div>
-
-      <div id="cadastro">
+            
+    </nav>
+    <div id="cadastro">
           <h1>Cadastro de Usúario</h1>
         <form method="post" action="" width="100%"> 
            
@@ -110,8 +111,6 @@
       </div>
     </div>
   </div>  
-            
-    </nav>
   </body>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
