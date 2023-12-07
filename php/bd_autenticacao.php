f4df61c49e40b19ba7bac32ebaf56eb6af4335c5
Laura Moreira <107573012+LauraMoreiraSilva@users.noreply.github.com>
2023-12-07 10:14:37 -0300

    Create bd_autenticacao.php
    Implementação de autenticação, banco de dados

---
+++ php/bd_autenticacao.php
@@ -0,0 +1,23 @@
+<?php
+// Receber dados do formulário de login
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// Importar o arquivo de conexão com o banco de dados
+require_once('../banco/conexao.php');
+
+// Verificar se o usuário existe no banco de dados
+// (Nota: A lógica de verificação pode variar dependendo da estrutura do seu banco de dados)
+$query = "SELECT * FROM usuarios WHERE username = '$username' AND password = '$password'";
+$result = mysqli_query($conexao, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    // Usuário existe, redirecionar para a página perfil.php
+    header("Location: perfil.php");
+    exit();
+} else {
+    // Usuário não existe, redirecionar para a página de login.php novamente
+    header("Location: ../login.php");
+    exit();
+}
+?>
