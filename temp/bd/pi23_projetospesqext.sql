4dd52c0b9e437affb2b62cfa3bfacce7b7d099e2
Ana Karolina <anak54497@gmail.com>
2023-12-13 13:47:41 -0300

    Atualização do banco de dados

--- temp/bd/pi23_projetospesqext.sql
+++ temp/bd/pi23_projetospesqext.sql
@@ -1,10 +1,5 @@
 CREATE DATABASE IF NOT EXISTS pi23_projetospesqext;
 USE pi23_projetospesqext;
-CREATE TABLE IF NOT EXISTS curso(
-id INT AUTO_INCREMENT PRIMARY KEY,
-nome VARCHAR(20),
-periodo INT NOT NULL
-);
 CREATE TABLE IF NOT EXISTS area(
 id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(20)
@@ -17,28 +12,13 @@ CREATE TABLE IF NOT EXISTS status(
 id INT AUTO_INCREMENT PRIMARY KEY,
 nome VARCHAR(20)
 );
-CREATE TABLE IF NOT EXISTS administrador(
-matricula VARCHAR(07) PRIMARY KEY NOT NULL,
-nome VARCHAR(200) NOT NULL,
-email VARCHAR(200) NOT NULL,
-senha VARCHAR (08) NOT NULL
-);
 CREATE TABLE IF NOT EXISTS docente(
 matricula VARCHAR(07) PRIMARY KEY NOT NULL,
 nome VARCHAR(200) NOT NULL,
+cpf VARCHAR(11) NOT NULL,
 email VARCHAR(200) NOT NULL,
 telefone VARCHAR(11) NOT NULL,
 senha VARCHAR (08) NOT NULL
-);
-CREATE TABLE IF NOT EXISTS discente(
-matricula VARCHAR(14) PRIMARY KEY NOT NULL,
-nome VARCHAR(200) NOT NULL,
-cpf VARCHAR(11) NOT NULL,
-rg VARCHAR(09) NOT NULL,
-email VARCHAR(100) NOT NULL,
-telefone VARCHAR(11),
-id_curso INT NOT NULL,
-FOREIGN KEY (id_curso) REFERENCES curso(id)
 );
 CREATE TABLE IF NOT EXISTS projeto(
 id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
@@ -53,22 +33,9 @@ FOREIGN KEY (id_status) REFERENCES status(id),
 FOREIGN KEY (id_area) REFERENCES area(id),
 FOREIGN KEY (id_categoria) REFERENCES categoria(id)
 );
-CREATE TABLE IF NOT EXISTS projeto_administrador(
-id_projeto INT NOT NULL,
-matricula_administrador VARCHAR(07),
-FOREIGN KEY (id_projeto) REFERENCES projeto(id),
-FOREIGN KEY (matricula_administrador) REFERENCES administrador(matricula)
-);
 CREATE TABLE IF NOT EXISTS projeto_docente(
 id_projeto INT NOT NULL,
 matricula_docente VARCHAR(07),
 FOREIGN KEY (id_projeto) REFERENCES projeto(id),
 FOREIGN KEY (matricula_docente) REFERENCES docente(matricula)
 );
-CREATE TABLE IF NOT EXISTS projeto_discente(
-id_projeto INT NOT NULL,
-matricula_discente VARCHAR(14),
-FOREIGN KEY (id_projeto) REFERENCES projeto(id),
-FOREIGN KEY (matricula_discente) REFERENCES discente(matricula)
-);
-
