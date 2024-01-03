869b00294236af876132fd9001f195de1688a02a
Ana Karolina <anak54497@gmail.com>
2024-01-03 13:27:25 -0300

    Correção da posição do rodapé da página de categorias

--- p_categorias.php
+++ p_categorias.php
@@ -6,61 +6,62 @@
 <head>
     <title>Categorias</title>
 
-    <!-- INCLUINDO CÓDIGO DE HEAD COMUM A TODAS AS PÁGINAS -->
     <?php include 'bases/head.php'; ?>
 
 </head>
 
 <body>
-  <!-- INCLUINDO CÓDIGO DE MENU COMUM A TODAS AS PÁGINAS -->
   <?php include 'bases/menu.php'; ?>
 
-    <div class="container">
+  <div class="container">
 
-<div class="row mt-5 mb-5">
-<div class="col-lg-6">
- <h2>Categorias</h2>
-<p><a href="p_categoria_adicionar.php">Adicionar</a></p>
+    <div class="row mt-5 mb-5">
+    <div class="col-lg-6">
+    <h2>Categorias</h2>
+    <p><a href="p_categoria_adicionar.php">Adicionar</a></p>
 
- <table class="table table-striped">
- <thead>
-  <tr>
-    <th scope="col">Nome</th>
-    <th scope="col">Editar</th>
-    <th scope="col">Remover</th>
-  </tr>
-</thead>
-<tbody>
+    <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Nome</th>
+        <th scope="col">Editar</th>
+        <th scope="col">Remover</th>
+      </tr>
+    </thead>
+    <tbody>
 
-<?php
+    <?php
 
-include "banco/conexao.php";
+    include "banco/conexao.php";
 
-$conn = conectar();
+    $conn = conectar();
 
-$sql = "SELECT * FROM categoria";
+    $sql = "SELECT * FROM categoria";
 
-$result = $conn->query($sql);
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-while($row = $result->fetch_assoc()) {
-  echo "<tr>";
-  echo "<td scope='row'>".$row["nome"]."</td>";
-  echo "<td><a href='php/p_categoria_editar.php?id=".$row["id"]."'>EDITAR</td></a>";
-  echo "<td><a href='php/p_categoria_remover_bd.php?id=".$row["id"]."'>REMOVER</td></a>";
-  echo "</tr>";
-}
-} else {
-echo "<td>Nenhuma categoria cadastrada</td><td></td><td></td>";
-}
+    if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      echo "<tr>";
+      echo "<td scope='row'>".$row["nome"]."</td>";
+      echo "<td><a href='php/p_categoria_editar.php?id=".$row["id"]."'>EDITAR</td></a>";
+      echo "<td><a href='php/p_categoria_remover_bd.php?id=".$row["id"]."'>REMOVER</td></a>";
+      echo "</tr>";
+    }
+    } else {
+    echo "<td>Nenhuma categoria cadastrada</td><td></td><td></td>";
+    }
 
-desconectar($conn);
+    desconectar($conn);
 
-?>
-  
-</tbody>
+    ?>
+      
+    </tbody>
 
-</table>
+    </table>
+      </div>
+    </div>
+  </div>
 
 </body>
 <?php include 'Bases/rodape.php';?>
